aacd34cdc8ced5daebd46861821e34dcbc6e9380
dan <md.erhenede@gmail.com>
2024-09-28 01:53:12 +0100

    first commit

---
+++ Client/app/index.tsx
@@ -0,0 +1,15 @@
+import { Stack, Link } from 'expo-router';
+
+import { Button } from '~/components/Button';
+import { Container } from '~/components/Container';
+import { Map } from '~/components/Map';
+import { ScreenContent } from '~/components/ScreenContent';
+
+export default function Home() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Home' }} />
+      <Map/>
+    </>
+  );
+}
---
+++ Client/components/Map.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native"
+import {MapView} from '@rnmapbox/maps'
+
+export const Map =()=>{
+    return(
+        <MapView/>
+    )
+}
